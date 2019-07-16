2f3396f570e1a21948203de6b9dafc4269e12d5d
Jesse Gorzinski <jgorzinski@gmail.com>
2019-07-16 08:57:21 -0500

    Another brute-force upgrade to include automake support for i

--- src/libuv/include/uv/unix.h
+++ src/libuv/include/uv/unix.h
@@ -49,8 +49,6 @@
 # include "uv/linux.h"
 #elif defined (__MVS__)
 # include "uv/os390.h"
-#elif defined(__PASE__)  /* __PASE__ and _AIX are both defined on IBM i */
-# include "uv/posix.h"  /* IBM i needs uv/posix.h, not uv/aix.h */
 #elif defined(_AIX)
 # include "uv/aix.h"
 #elif defined(__sun)
@@ -63,7 +61,8 @@
       defined(__OpenBSD__)         || \
       defined(__NetBSD__)
 # include "uv/bsd.h"
-#elif defined(__CYGWIN__) || \
+#elif defined(__PASE__)   || \
+      defined(__CYGWIN__) || \
       defined(__MSYS__)   || \
       defined(__GNU__)
 # include "uv/posix.h"
